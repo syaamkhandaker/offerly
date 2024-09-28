81591e24d8320f22e72a6801e5145294a9fa8179
henqc <henryq.chen101@gmail.com>
2024-09-27 22:54:52 -0400

    hero page basic

---
+++ src/components/nav/index.tsx
@@ -0,0 +1,10 @@
+export default function Nav() {
+  return (
+    <div className="flex justify-between p-10 items-center">
+      <div className="text-xl">Logo</div>
+      <button className="border border-solid px-4 py-2 text-xl rounded-md">
+        Sign In
+      </button>
+    </div>
+  );
+}
