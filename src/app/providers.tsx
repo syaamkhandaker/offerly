e031f844c3eb6a8754569b8de7ee6be7d0e1e7cc
Syaam Khandaker <syaamkhandaker@gmail.com>
2024-09-28 23:56:55 -0400

    removed chakra theme and provider

--- src/app/providers.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { ChakraProvider } from "@chakra-ui/react";
-import customTheme from "./theme";
-
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider theme={customTheme}>{children}</ChakraProvider>;
-}
